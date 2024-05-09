da353a3dae142b219ad76ffdfa2a906f43371c30
rithasahmed12 <ahmedrithas48@gmail.com>
2024-05-09 22:07:35 +0530

    started register

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import {ConfigModule} from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:'.env',
+      isGlobal:true
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
