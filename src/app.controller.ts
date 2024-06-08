0a0b1d04782f83b9751ae786cd6d0fe29068f7a9
rithasahmed12 <ahmedrithas48@gmail.com>
2024-06-08 21:17:48 +0530

    checking multi tenancey

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() req:Request) {
+    var domain = req.get('host').match(/\w+/); // e.g., host: "subdomain.website.com"
+    if (domain)
+       var subdomain = domain[0]; // Use "subdomain"
+      console.log(domain,subdomain);
+      
   }
 }
