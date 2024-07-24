68554c97e779e11adacecef77eda05bee5a47f7a
rithasahmed12 <ahmedrithas48@gmail.com>
2024-07-24 16:40:26 +0530

    handled the subdomain part of cors

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,36 @@ import checkOrigin from './checkOrigin';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{bodyParser:false});
 
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      
+      const allowedOrigins = [
+        /^https?:\/\/(.+\.)?shoetopia\.site$/,  // Matches your domain and all subdomains
+        /^http:\/\/(.+\.)?localhost:3000$/,     // Matches localhost and all subdomains
+        'https://shoetopia.site'                // Allows requests from your main domain
+      ];
+      
+      const allowed = allowedOrigins.some(allowedOrigin => {
+        if (typeof allowedOrigin === 'string') {
+          return origin === allowedOrigin;
+        } else if (allowedOrigin instanceof RegExp) {
+          return allowedOrigin.test(origin);
+        }
+      });
+      
+      if (allowed) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  };
+
   app.use(cookieParser());
-  app.enableCors({
-    origin: checkOrigin,
-    credentials: true,
-  });
+  app.enableCors(corsOptions);
+
 
   app.useGlobalPipes(new ValidationPipe());
 
